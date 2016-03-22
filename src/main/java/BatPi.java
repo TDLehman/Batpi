46a818da09b58d4613fc85bfaac472b48d3e2f56
Tylor Lehman <lehmantd197@potsdam.edu>
2016-03-22 15:39:15 -0400

    Work on the RadarMap
    Overlaying layers prevent each other from being visible

--- src/main/java/BatPi.java
+++ src/main/java/BatPi.java
@@ -1,43 +1,28 @@
 //Tylor Lehman
 //BatPi
 //BatPi
-import java.io.*;
-//import org.python.core.PyException;
-//import org.python.core.PyInteger;
-//import org.python.core.PyObject;
-//import org.python.util.PythonInterpreter;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Arrays;
 
 public class BatPi{
    public static void main(String[] args){
-      String s = "";
-      if (args[0] != null){
+      if (args.length == 0){
          RadarMap map = new RadarMap();
+         sleep(250);
       }
-      try{
-         //Process p = Runtime.getRuntime().exec("python3 ./main/python/pyTest.py");
-         Process p = Runtime.getRuntime().exec("ls");
-         BufferedReader stdInput = new BufferedReader(new
-                 InputStreamReader(p.getInputStream()));   
          
-         
-         //read output from the python command (I hope):
-         
-         System.out.println("Here is your output:\n");
-         
-         while ((s = stdInput.readLine()) != null){
-            System.out.println(s);
-         }
-         
-      }catch(IOException e){
-         System.out.print("Exception happened - Here's what I know: \n");
-         e.printStackTrace();
-         System.exit(-1);
+      
+      
+   }
+   public static void sleep(int length){
+      try {
+         Thread.sleep(length);
+      } catch (InterruptedException e) {
+         System.out.println("Ignoring a thread Exception");
       }
       
-
-      
-      
-      
+         
    }
 
 }
--- src/main/java/RadarMap.java
+++ src/main/java/RadarMap.java
@@ -13,21 +13,108 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-public class RadarMap extends JFrame{
+public class RadarMap{
    private JFrame radarmap;
    
-   // Constructs radarmap. Used for client display.
+
    public RadarMap(){
-      map = new JFrame("RadarMap");
-      map.setDefaultCloseOperation(JFram.EXIT_ON_CLOSE);
-      map.setSize(1000,700);
+      JFrame map = new JFrame("RadarMap");
+      map.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      map.setSize(600,400);
       map.setLocationRelativeTo(null);
-      
+      map.add(makePanel());
+      map.add(makeLines());
+      map.setVisible(true);
    }
    
-   
-   
-   
-   
+   public static JPanel makePanel(){
+      JPanel panel = new JPanel();
+      panel.setLayout(new BorderLayout());
+      panel.setOpaque(true);
+      int xdim=600;
+      int ydim=400;
+      panel.setPreferredSize(new Dimension(xdim, ydim));
+      panel.setBackground(Color.BLACK);
+      Origin o = new Origin();
+      panel.add(o);
+      //LinesComponent comp = new LinesComponent();
+      //comp.setPreferredSize(new Dimension(320, 200));
+      //comp.addLine(xdim/2, ydim-(ydim/8), 30, 30, Color.GREEN);
+      //comp.addLine(xdim/2, ydim-(ydim/8), 500, 30, Color.GREEN);
+      //panel.add(comp);
+      return panel;
+   }
+
+   public static JPanel makeLines(){
+      JPanel panel = new JPanel();
+      panel.setLayout(new BorderLayout());
+      panel.setBackground(Color.BLACK);
+      panel.setOpaque(true);
+      int xdim=600;
+      int ydim=400;
+      panel.setPreferredSize(new Dimension(xdim, ydim));
+ 
+      //Origin o = new Origin();
+      //panel.add(o);
+      LinesComponent comp = new LinesComponent();
+      comp.setPreferredSize(new Dimension(320, 200));
+      comp.addLine(xdim/2, ydim-(ydim/8), 30, 30, Color.GREEN);
+      comp.addLine(xdim/2, ydim-(ydim/8), 500, 30, Color.GREEN);
+      panel.add(comp);
+      return panel;
+   }
+}
+
+
+
+
+class Origin extends JComponent{
+   public void paintComponent(Graphics g){
+      super.paintComponent(g);
+      g.setColor(Color.GREEN);
+      g.fillOval(getWidth()/2, getHeight()-(getHeight()/8), 20, 20); 
+        
+   }
+}
+
+class LinesComponent extends JComponent{
+private static class Line{
+   final int x1;
+   final int y1;
+   final int x2;
+   final int y2;
+   final Color color;
+   public Line(int x1, int y1, int x2, int y2, Color color){
+      this.x1 = x1;
+      this.y1 = y1;
+      this.x2 = x2;
+      this.y2 = y2;
+      this.color = color;
+   }
+}
+private final LinkedList<Line> lines = new LinkedList<Line>();
+
+public void addLine(int x1, int x2, int x3, int x4) {
+    addLine(x1, x2, x3, x4, Color.black);
+}
+
+public void addLine(int x1, int x2, int x3, int x4, Color color) {
+    lines.add(new Line(x1,x2,x3,x4, color));        
+    repaint();
+}
+
+public void clearLines() {
+    lines.clear();
+    repaint();
+}
+
+@Override
+protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    for (Line line : lines) {
+        g.setColor(line.color);
+        g.drawLine(line.x1, line.y1, line.x2, line.y2);
+    }
+}
 
 }
