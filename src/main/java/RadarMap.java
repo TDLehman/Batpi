5b96b59c90ab85b348506030d37ad2c2d99e4d0c
Tylor Lehman <tylorlehman@yahoo.com>
2016-03-23 17:44:36 -0400

    Make radarmap display an arbitrary range
    Near Future:
    	Decide on number of samples,
    	write function to decide shape of polygons denoting range
    	make work awesome

--- src/main/java/RadarMap.java
+++ src/main/java/RadarMap.java
@@ -37,9 +37,6 @@ public class RadarMap{
       int xdim=600;
       int ydim=400;
       panel.setPreferredSize(new Dimension(xdim, ydim));
- 
-      //Origin o = new Origin();
-      //panel.add(o);
       LinesComponent comp = new LinesComponent();
       comp.setPreferredSize(new Dimension(320, 200));
       comp.addLine(xdim/2, ydim-(ydim/8), 10, 60, Color.GREEN);
@@ -51,49 +48,53 @@ public class RadarMap{
 
 
 class LinesComponent extends JComponent{
-private static class Line{
-   final int x1;
-   final int y1;
-   final int x2;
-   final int y2;
-   final Color color;
-   public Line(int x1, int y1, int x2, int y2, Color color){
-      this.x1 = x1;
-      this.y1 = y1;
-      this.x2 = x2;
-      this.y2 = y2;
-      this.color = color;
+   private static class Line{
+      final int x1;
+      final int y1;
+      final int x2;
+      final int y2;
+      final Color color;
+
+      public Line(int x1, int y1, int x2, int y2, Color color){
+         this.x1 = x1;
+         this.y1 = y1;
+         this.x2 = x2;
+         this.y2 = y2;
+         this.color = color;
+      }
    }
-}
-private final LinkedList<Line> lines = new LinkedList<Line>();
 
-public void addLine(int x1, int x2, int x3, int x4) {
-    addLine(x1, x2, x3, x4, Color.GREEN);
-}
+   private final LinkedList<Line> lines = new LinkedList<Line>();
 
-public void addLine(int x1, int x2, int x3, int x4, Color color) {
-    lines.add(new Line(x1,x2,x3,x4, color));        
-    repaint();
-}
+   public void addLine(int x1, int x2, int x3, int x4) {
+      addLine(x1, x2, x3, x4, Color.GREEN);
+      repaint();
+   }
 
-public void clearLines() {
-    lines.clear();
-    repaint();
-}
+   public void addLine(int x1, int x2, int x3, int x4, Color color) {
+      lines.add(new Line(x1,x2,x3,x4, color));        
+      repaint();
+   }
 
-@Override
-protected void paintComponent(Graphics g) {
-    super.paintComponent(g);
-    int x = 0; //Simply keeps track of number of lines
-    for (Line line : lines) {
-      g.setColor(line.color);
-      g.drawLine(line.x1, line.y1, line.x2, line.y2);
-      if (x == 0)
-         g.fillOval(line.x1-5, line.y1-5, 10, 10);
-      x++;
-    }
-    
-//    g.fillOval(getWidth()/2, getHeight()-(getHeight()/8), 10, 10);
-}
+   public void clearLines() {
+      lines.clear();
+      repaint();
+   }
 
+   @Override
+   protected void paintComponent(Graphics g) {
+      super.paintComponent(g);
+      int[] a1 = {300, 200, 400};
+      int[] a2 = {350, 200, 200};
+      int x = 0; //Simply keeps track of number of lines
+      for (Line line : lines) {
+            g.setColor(line.color);
+            g.drawLine(line.x1, line.y1, line.x2, line.y2);
+         if (x == 0){
+            g.fillOval(line.x1-5, line.y1-5, 10, 10);
+            g.fillPolygon(a1, a2, 3);
+            x++;
+         }
+      }
+   }
 }
