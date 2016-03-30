8e7af94af866a32ff2bf7cf3d1771dafcc962900
Tylor Lehman <TylorLehman@gmail.com>
2016-03-30 15:05:51 -0400

    Change function of servo drive for blaster
    Servodrive now functions with servoblaster utility, instead of separate raspbian library

--- src/main/python/ServoDrive.py
+++ src/main/python/ServoDrive.py
@@ -2,40 +2,50 @@
 # BatPi
 # ServoDrive.py
 
-# set: The first argument to this is the file to be written to (property) and the second argument the value to write to it.
+# the servod daemon from ./servoblaster/PiBits/servoblaster/
+# It sets up a named pipe device named /dev/servoblaster.
+# Writing pulse width to /dev/servoblaster actuates the servo.
+#
+# Example: echo 0=150 > /dev/servoblaster
+# The example means send pulse width of 150 steps (each step is 10us, so pulse width is 1500us or 1.5ms)
+# to servo #0 (which by default should be connected to pin 7, unless the defaults are overridden from servod command line).
+# Note that 'sudo' is not required because all users have write permission on /dev/servoblaster
+#
+# The list of default pins and servo numbers are shown below. What this means is if you connect
+# servo to board pin 7, that servo can be addresses as '0'.
+#      Servo number    GPIO number   Pin in P1 header
+#          0               4             P1-7
+#          1              17             P1-11
+#          2              18             P1-12
+#          3             21/27           P1-13
+#          4              22             P1-15
+#          5              23             P1-16
+#          6              24             P1-18
+#          7              25             P1-22
 
-# the program starts with a few file writes to turn the delay mode off, set the mode to be 'servo', set the maximum servo value to be 180 and finally to make the output pin active.
+from time import sleep
 
-# A variable (delay_period) is used to contain the time in seconds between each step of the servo.
+hold_position_time = 0.1 # secs
+step_size = 10 # ie, change pulse width by 10*10us = 100us or 0.1ms in each step
 
-# The while loop will just continue forever. Within the loop there are two near identical 'for' loops. The first counts the angle up from 0 to 180 and the second sets the servo angle to 180 – angle, moving the servo arm back from 180 to 0 degrees.
+# Dont buffer the writes to the device file, to avoid explicit flush()es.
+dont_buffer = 0
 
-#----------------------------------------------------------------------
+with open('/dev/servoblaster', "w", dont_buffer ) as servo_blaster_device:
+    while True:
+        for pulse_width in range(100,200,step_size):
+            # The \n is required. Without it, it doesn't work.
+            cmd = "0=" + str(pulse_width) + "\n"
+            print cmd
+            servo_blaster_device.write(cmd)
+            # flush() is required if buffering is enabled, which by default is.
+            #servo_blaster_device.flush()
+            sleep(hold_position_time)
 
-import time
-def set(property, value):
-	try:
-		f = open("/sys/class/rpi-pwm/pwm0/" + property, 'w')
-		f.write(value)
-		f.close()	
-	except:
-		print("Error writing to: " + property + " value: " + value)
-     
-def setServo(angle):
-	set("servo", str(angle))
-	
-	
-set("delayed", "0")
-set("mode", "servo")
-set("servo_max", "180")
-set("active", "1")
-     
-delay_period = 0.01
-     
-while True:
-	for angle in range(0, 180):
-		setServo(angle)
-		time.sleep(delay_period)
-	for angle in range(0, 180):
-		setServo(180 - angle)
-		time.sleep(delay_period)
+        for pulse_width in range(200,100,-step_size):
+            cmd = "0=" + str(pulse_width) + "\n"
+            print cmd
+            servo_blaster_device.write(cmd)
+            #servo_blaster_device.flush()
+            sleep(hold_position_time)
+share i
