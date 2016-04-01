d811bb08cc5114af41b5fd6ab7113dc76ef83262
Tylor Lehman <tylorlehman@yahoo.com>
2016-04-01 10:07:25 -0400

    Rename testfile.py and add comment

---
+++ src/main/python/fullsweep.py
@@ -0,0 +1,27 @@
+# Tylor Lehman
+# BatPi
+# fullsweep.py
+
+
+
+
+# fullsweep currently takes 10 average samples and stores them in array li
+# and then prints array li. It should probably be returned to something later
+# _____________________________________________________________________________ 
+from Echolocate import getSample
+from time import sleep
+
+
+def fullsweep():
+   step_size = 10 # change pulse width by 10*10us = 100us or 0.1ms in each step
+   dont_buffer = 0 # don't buffer writes to avoid flushes
+   samparray = [0 for i in range(float(100)/step_size)] #float bc remainder
+   li = 0 # Loop counter
+   with open('/dev/servoblaster', "w", dont_buffer ) as servo_blaster_device:
+      for pulse_width in range(100,200,step_size):
+         cmd = "0=" + str(pulse_width) + "\n"
+         print cmd
+         servo_blaster_device.write(cmd)
+         samparray[li] = getSample()
+         sleep(.25) # This might be way too big of a sleep
+   print (li)
