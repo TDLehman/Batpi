ce0d78ef6af43124837a39391b3ece0c53fe59de
Tylor Lehman <tylorlehman@yahoo.com>
2016-04-06 11:27:44 -0400

    Make PiDrive a class with various functions
    Functions added:
    -forward()
    -backward()
    -stop()
    -turnRight()
    -turnLeft()

--- src/main/python/PiDrive.py
+++ src/main/python/PiDrive.py
@@ -7,52 +7,80 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-# Should be switched to BCM for consistency purposes.
-# See Echolocate.py 
-# GPIO.setmode(GPIO.BOARD)
-GPIO.setmode(GPIO.BCM) 
-# Determine available Pins ahead of time.
-# Correct pins If I'm using the B+, which I THINK I AM
-Motor1A = 17 # GPIO 17 (pin 11)
-Motor1B = 27 # GPIO 27 (pin 13) 
-Motor1E = 22 # GPIO 22 (Pin 15)
- 
-Motor2A = 5 # GPIO 5 (Pin 29)
-Motor2B = 6 # GPIO 6 (Pin 31)
-Motor2E = 13 # GPIO 13 (Pin 33)
- 
-GPIO.setup(Motor1A,GPIO.OUT)
-GPIO.setup(Motor1B,GPIO.OUT)
-GPIO.setup(Motor1E,GPIO.OUT)
- 
-GPIO.setup(Motor2A,GPIO.OUT)
-GPIO.setup(Motor2B,GPIO.OUT)
-GPIO.setup(Motor2E,GPIO.OUT)
- 
-print "Going forwards"
-GPIO.output(Motor1A,GPIO.HIGH)
-GPIO.output(Motor1B,GPIO.LOW)
-GPIO.output(Motor1E,GPIO.HIGH)
- 
-GPIO.output(Motor2A,GPIO.HIGH)
-GPIO.output(Motor2B,GPIO.LOW)
-GPIO.output(Motor2E,GPIO.HIGH)
- 
-sleep(2)
- 
-print "Going backwards"
-GPIO.output(Motor1A,GPIO.LOW)
-GPIO.output(Motor1B,GPIO.HIGH)
-GPIO.output(Motor1E,GPIO.HIGH)
- 
-GPIO.output(Motor2A,GPIO.LOW)
-GPIO.output(Motor2B,GPIO.HIGH)
-GPIO.output(Motor2E,GPIO.HIGH)
- 
-sleep(2)
- 
-print "Now stop"
-GPIO.output(Motor1E,GPIO.LOW)
-GPIO.output(Motor2E,GPIO.LOW)
- 
-GPIO.cleanup()
+
+
+class PiDrive(object):
+   def __init__(self):
+      GPIO.setmode(GPIO.BCM) 
+      Motor1A = 17 # GPIO 17 (pin 11)
+      Motor1B = 27 # GPIO 27 (pin 13) 
+      Motor1E = 22 # GPIO 22 (Pin 15)
+       
+      Motor2A = 5 # GPIO 5 (Pin 29)
+      Motor2B = 6 # GPIO 6 (Pin 31)
+      Motor2E = 13 # GPIO 13 (Pin 33)
+
+      GPIO.setup(Motor1A,GPIO.OUT)
+      GPIO.setup(Motor1B,GPIO.OUT)
+      GPIO.setup(Motor1E,GPIO.OUT)
+       
+      GPIO.setup(Motor2A,GPIO.OUT)
+      GPIO.setup(Motor2B,GPIO.OUT)
+      GPIO.setup(Motor2E,GPIO.OUT)
+
+
+   def forward() 
+      print "Going forward 1"
+      GPIO.output(Motor1A,GPIO.HIGH)
+      GPIO.output(Motor1B,GPIO.LOW)
+      GPIO.output(Motor1E,GPIO.HIGH)
+      
+      GPIO.output(Motor2A,GPIO.HIGH)
+      GPIO.output(Motor2B,GPIO.LOW)
+      GPIO.output(Motor2E,GPIO.HIGH)
+      sleep(1)
+      stop() 
+      
+   def backward() 
+      print "Going backward 1"
+      GPIO.output(Motor1A,GPIO.LOW)
+      GPIO.output(Motor1B,GPIO.HIGH)
+      GPIO.output(Motor1E,GPIO.HIGH)
+       
+      GPIO.output(Motor2A,GPIO.LOW)
+      GPIO.output(Motor2B,GPIO.HIGH)
+      GPIO.output(Motor2E,GPIO.HIGH)
+      sleep(1)
+      stop()
+
+   def stop() 
+      print "Stopping"
+      GPIO.output(Motor1E,GPIO.LOW)
+      GPIO.output(Motor2E,GPIO.LOW)
+      GPIO.cleanup()
+    
+   def turnRight()
+      print "Turning Right"
+      #Turn left wheel forward
+      GPIO.output(Motor1A,GPIO.HIGH)
+      GPIO.output(Motor1B,GPIO.LOW)
+      GPIO.output(Motor1E,GPIO.HIGH)   
+      #Turn right wheel backward
+      GPIO.output(Motor2A,GPIO.LOW)
+      GPIO.output(Motor2B,GPIO.HIGH)
+      GPIO.output(Motor2E,GPIO.HIGH)
+      sleep(.25)
+      stop()
+
+   def turnLeft()
+      print "Turning Left"
+      # Turn left wheel backward
+      GPIO.output(Motor1A,GPIO.LOW)
+      GPIO.output(Motor1B,GPIO.HIGH)
+      GPIO.output(Motor1E,GPIO.HIGH)
+      # Turn right wheel forward
+      GPIO.output(Motor2A,GPIO.HIGH)
+      GPIO.output(Motor2B,GPIO.LOW)
+      GPIO.output(Motor2E,GPIO.HIGH)
+      sleep(.25)
+      stop()
