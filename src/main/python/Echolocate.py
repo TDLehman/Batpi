9f73c1441801b62848e92f5f0fa527ece899984a
Tylor Lehman <tylorlehman@yahoo.com>
2016-04-06 18:33:03 -0400

    fix class structure of Echolocate

--- src/main/python/Echolocate.py
+++ src/main/python/Echolocate.py
@@ -7,50 +7,49 @@ import time
 
 
 # Setmode might have to go inside of function. idk.
+class echolocate(object):
+   def __init__(self):
+      GPIO.setmode(GPIO.BCM) # Set easy pin numbering system
+      GPIO.setwarnings(False) # Ignore stupid warnings
+      trig = 23 # GPIO 23 (Pin 16), Connects to Sonar Sensor trigger
+      echo = 24 # GPIO 24 (pin 18), connects to Sonar Sensor Echo
+      GPIO.setup(trig, GPIO.OUT) # set GPIO ports as either output or input
+      GPIO.setup(echo, GPIO.IN)
 
-def getSample():
-   GPIO.setmode(GPIO.BCM) # Set easy pin numbering system
-   GPIO.setwarnings(False) # Ignore stupid warnings
-   trig = 23 # GPIO 23 (Pin 16), Connects to Sonar Sensor trigger
-   echo = 24 # GPIO 24 (pin 18), connects to Sonar Sensor Echo
-   GPIO.setup(trig, GPIO.OUT) # set GPIO ports as either output or input
-   GPIO.setup(echo, GPIO.IN)
+   def getSample():
+      x = takeSample(trig, echo)
+      y = takeSample(trig, echo)
+      z = takeSample(trig, echo)
 
-   x = takeSample(trig, echo)
-   y = takeSample(trig, echo)
-   z = takeSample(trig, echo)
-
-   while max(x,y,z) > 2*min(x,y,z):
-      print ("A measurement stunk. :(")
-      if x == max(x,y,z):
+      while max(x,y,z) > 2*min(x,y,z):
+         print ("Outlier Detected. Retaking Samples")
          x = takeSample(trig, echo)
-      elif y == max(x,y,z):
          y = takeSample(trig, echo)
-      elif z == max(x,y,z):
          z = takeSample(trig, echo)
 
-   avDistance = (x+y+z)/float(2)
-   return avDistance
+      avDistance = (x+y+z)/float(3)
+      return avDistance
 
 
-def takeSample(trig, echo):
-   print "Once"
-   GPIO.setmode(GPIO.BCM)
-   GPIO.setup(trig, GPIO.OUT) # set GPIO ports as either output or input
-   GPIO.setup(echo, GPIO.IN)
-   GPIO.output(trig,  False) # set trigger pin to low,
-   time.sleep(.5) # wait for it to settle
-   GPIO.output(trig, True)
-   time.sleep(0.00001) # HC - SR04 documentation, set trig high 10uS.
-   GPIO.output(trig, False)
-   while GPIO.input(echo) == 0: # trigger sets echo pin to high
-     start = time.time() # Keeps getting stuck here! *********************
-   while GPIO.input(echo) == 1: # echo goes low when signal is returned
-     end = time.time() # Python returns last time echo was high 
-   duration = end - start # measure the time the pin stayed high
-   soundSpeed = 34300 # Speed of soundwave
-   distance = duration*34300/2 # account for return trip
-   # reset pins, return distance to nearest object
-   GPIO.cleanup()
-   return distance
-   
+   def takeSample(trig, echo):
+      # Uncomment for error checking
+      #print "Once"
+      GPIO.setmode(GPIO.BCM)
+      GPIO.setup(trig, GPIO.OUT) # set GPIO ports as either output or input
+      GPIO.setup(echo, GPIO.IN)
+      GPIO.output(trig,  False) # set trigger pin to low,
+      time.sleep(.5) # wait for it to settle
+      GPIO.output(trig, True)
+      time.sleep(0.00001) # HC - SR04 documentation, set trig high 10uS.
+      GPIO.output(trig, False)
+      while GPIO.input(echo) == 0: # trigger sets echo pin to high
+        start = time.time() # Keeps getting stuck here! 
+      while GPIO.input(echo) == 1: # echo goes low when signal is returned
+        end = time.time() # Python returns last time echo was high 
+      duration = end - start # measure the time the pin stayed high
+      soundSpeed = 34300 # Speed of soundwave
+      distance = duration*34300/2 # account for return trip
+      # reset pins, return distance to nearest object
+      GPIO.cleanup()
+      return distance
+      
