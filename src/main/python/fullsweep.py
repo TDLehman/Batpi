3ae925d8214a7a76b9bbb5cc4c668fe023d1f0fa
Tylor Lehman <tylorlehman@yahoo.com>
2016-04-06 18:33:26 -0400

    Modify class structure of fullsweep.py

--- src/main/python/fullsweep.py
+++ src/main/python/fullsweep.py
@@ -13,18 +13,21 @@ from Echolocate import getSample
 from time import sleep
 
 
-def fullsweep():
-   step_size = 10 # change pulse width by 10*10us = 100us or 0.1ms in each step
-   dont_buffer = 0 # don't buffer writes to avoid flushes
-   samparray = [0 for i in range(200/step_size)]
-   li = 0 # Loop counter
-   with open('/dev/servoblaster', "w", dont_buffer ) as servo_blaster_device:
-      for pulse_width in range(50,250,step_size):
-         cmd = "0=" + str(pulse_width) + "\n"
-         print cmd
-         servo_blaster_device.write(cmd)
-         samparray[li] = getSample()
-         #sleep(.25) # This might be way too big of a sleep
-   print (li)
+class fullsweep():
+   def __init__(self):
+      step_size = 10 # change pulse width 10*10us = 100us or 0.1ms in each step
+      dont_buffer = 0 # don't buffer writes to avoid flushes
+      samparray = [0 for i in range(200/step_size)]
+      sweep()
+      print (samparray)
 
-fullsweep()
+
+   def sweep():
+      li = 0 # Loop counter
+      with open('/dev/servoblaster', "w", dont_buffer ) as servo_blaster_device:
+         for pulse_width in range(50,250,step_size):
+            cmd = "0=" + str(pulse_width) + "\n"
+            print cmd
+            servo_blaster_device.write(cmd)
+            self.samparray[li] = getSample()
+            #sleep(.25) # This might be way too big of a sleep
