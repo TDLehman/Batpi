ebbbd33ee81dd096aea46b55e02fdcbe470ed250
TylorLehman <tylorlehman@yahoo.com>
2016-04-07 16:11:42 +0000

    Work on fullsweep. access denied

--- src/main/python/fullsweep.py
+++ src/main/python/fullsweep.py
@@ -9,25 +9,27 @@
 # and then prints array li. It should probably be returned to something later
 #
 # Gets stuck in echolocate sometimes _____________________________________________________________________________ 
-from Echolocate import getSample
+from Echolocate import Echolocate
 from time import sleep
 
 
-class fullsweep():
+class fullsweep(object):
    def __init__(self):
-      step_size = 10 # change pulse width 10*10us = 100us or 0.1ms in each step
-      dont_buffer = 0 # don't buffer writes to avoid flushes
-      samparray = [0 for i in range(200/step_size)]
-      sweep()
-      print (samparray)
-
-
-   def sweep():
+      self.step_size = 10 # change pulse width 10*10us = 100us or 0.1ms in each step
+      self.dont_buffer = 0 # don't buffer writes to avoid flushes
+      self.samparray = [0 for i in range(200/self.step_size)]
+      self.locator = Echolocate()
+      
+   def sweep(self):
       li = 0 # Loop counter
-      with open('/dev/servoblaster', "w", dont_buffer ) as servo_blaster_device:
-         for pulse_width in range(50,250,step_size):
+      with open('/dev/servoblaster', "w", self.dont_buffer ) as servo_blaster_device:
+         for pulse_width in range(50,250,self.step_size):
             cmd = "0=" + str(pulse_width) + "\n"
             print cmd
             servo_blaster_device.write(cmd)
-            self.samparray[li] = getSample()
+            self.samparray[li] = self.locator.getSample()
             #sleep(.25) # This might be way too big of a sleep
+
+if __name__ == "__main__":
+   x=fullsweep()
+   x.sweep()
