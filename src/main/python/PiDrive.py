2dc395ea1d7ecf60a0227b9e1a5f237a3caf64f1
TylorLehman <tylorlehman@yahoo.com>
2016-04-07 15:34:03 +0000

    Fix compile errors in PiDrive

--- src/main/python/PiDrive.py
+++ src/main/python/PiDrive.py
@@ -11,87 +11,88 @@ from time import sleep
 
 class PiDrive(object):
    def __init__(self):
+      GPIO.setwarnings(False)
       GPIO.setmode(GPIO.BCM) 
-      Motor1A = 17 # GPIO 17 (pin 11)
-      Motor1B = 27 # GPIO 27 (pin 13) 
-      Motor1E = 22 # GPIO 22 (Pin 15)
+      self.Motor1A = 17 # GPIO 17 (pin 11)
+      self.Motor1B = 27 # GPIO 27 (pin 13) 
+      self.Motor1E = 22 # GPIO 22 (Pin 15)
        
-      Motor2A = 5 # GPIO 5 (Pin 29)
-      Motor2B = 6 # GPIO 6 (Pin 31)
-      Motor2E = 13 # GPIO 13 (Pin 33)
+      self.Motor2A = 5 # GPIO 5 (Pin 29)
+      self.Motor2B = 6 # GPIO 6 (Pin 31)
+      self.Motor2E = 13 # GPIO 13 (Pin 33)
 
-      GPIO.setup(Motor1A,GPIO.OUT)
-      GPIO.setup(Motor1B,GPIO.OUT)
-      GPIO.setup(Motor1E,GPIO.OUT)
+      GPIO.setup(self.Motor1A,GPIO.OUT)
+      GPIO.setup(self.Motor1B,GPIO.OUT)
+      GPIO.setup(self.Motor1E,GPIO.OUT)
        
-      GPIO.setup(Motor2A,GPIO.OUT)
-      GPIO.setup(Motor2B,GPIO.OUT)
-      GPIO.setup(Motor2E,GPIO.OUT)
+      GPIO.setup(self.Motor2A,GPIO.OUT)
+      GPIO.setup(self.Motor2B,GPIO.OUT)
+      GPIO.setup(self.Motor2E,GPIO.OUT)
 
 
-   def forward():
+   def forward(self):
       print "Going forward 1"
-      GPIO.output(Motor1A,GPIO.HIGH)
-      GPIO.output(Motor1B,GPIO.LOW)
-      GPIO.output(Motor1E,GPIO.HIGH)
+      GPIO.output(self.Motor1A,GPIO.HIGH)
+      GPIO.output(self.Motor1B,GPIO.LOW)
+      GPIO.output(self.Motor1E,GPIO.HIGH)
       
-      GPIO.output(Motor2A,GPIO.HIGH)
-      GPIO.output(Motor2B,GPIO.LOW)
-      GPIO.output(Motor2E,GPIO.HIGH)
+      GPIO.output(self.Motor2A,GPIO.HIGH)
+      GPIO.output(self.Motor2B,GPIO.LOW)
+      GPIO.output(self.Motor2E,GPIO.HIGH)
       sleep(1)
-      stop() 
+      self.stop() 
       
-   def backward(): 
+   def backward(self): 
       print "Going backward 1"
-      GPIO.output(Motor1A,GPIO.LOW)
-      GPIO.output(Motor1B,GPIO.HIGH)
-      GPIO.output(Motor1E,GPIO.HIGH)
+      GPIO.output(self.Motor1A,GPIO.LOW)
+      GPIO.output(self.Motor1B,GPIO.HIGH)
+      GPIO.output(self.Motor1E,GPIO.HIGH)
        
-      GPIO.output(Motor2A,GPIO.LOW)
-      GPIO.output(Motor2B,GPIO.HIGH)
-      GPIO.output(Motor2E,GPIO.HIGH)
+      GPIO.output(self.Motor2A,GPIO.LOW)
+      GPIO.output(self.Motor2B,GPIO.HIGH)
+      GPIO.output(self.Motor2E,GPIO.HIGH)
       sleep(1)
-      stop()
+      self.stop()
 
     
-   def turnRight():
+   def turnRight(self):
       print "Turning Right"
       #Turn left wheel forward
-      GPIO.output(Motor1A,GPIO.HIGH)
-      GPIO.output(Motor1B,GPIO.LOW)
-      GPIO.output(Motor1E,GPIO.HIGH)   
+      GPIO.output(self.Motor1A,GPIO.HIGH)
+      GPIO.output(self.Motor1B,GPIO.LOW)
+      GPIO.output(self.Motor1E,GPIO.HIGH)   
       #Turn right wheel backward
-      GPIO.output(Motor2A,GPIO.LOW)
-      GPIO.output(Motor2B,GPIO.HIGH)
-      GPIO.output(Motor2E,GPIO.HIGH)
-      sleep(.25)
-      stop()
+      GPIO.output(self.Motor2A,GPIO.LOW)
+      GPIO.output(self.Motor2B,GPIO.HIGH)
+      GPIO.output(self.Motor2E,GPIO.HIGH)
+      sleep(.50)
+      self.stop()
 
-   def turnLeft():
+   def turnLeft(self):
       print "Turning Left"
       # Turn left wheel backward
-      GPIO.output(Motor1A,GPIO.LOW)
-      GPIO.output(Motor1B,GPIO.HIGH)
-      GPIO.output(Motor1E,GPIO.HIGH)
+      GPIO.output(self.Motor1A,GPIO.LOW)
+      GPIO.output(self.Motor1B,GPIO.HIGH)
+      GPIO.output(self.Motor1E,GPIO.HIGH)
       # Turn right wheel forward
-      GPIO.output(Motor2A,GPIO.HIGH)
-      GPIO.output(Motor2B,GPIO.LOW)
-      GPIO.output(Motor2E,GPIO.HIGH)
-      sleep(.25)
-      stop()
+      GPIO.output(self.Motor2A,GPIO.HIGH)
+      GPIO.output(self.Motor2B,GPIO.LOW)
+      GPIO.output(self.Motor2E,GPIO.HIGH)
+      sleep(.50)
+      self.stop()
 
-   def stop(): 
+   def stop(self): 
       print "Stopping"
-      GPIO.output(Motor1E,GPIO.LOW)
-      GPIO.output(Motor2E,GPIO.LOW)
-      GPIO.cleanup()
+      GPIO.output(self.Motor1E,GPIO.LOW)
+      GPIO.output(self.Motor2E,GPIO.LOW)
+      #GPIO.cleanup()
 
 
 
 if __name__ == "__main__":
-   x=PiDrive
-   forward()
-   backward()
-   turnLeft()
-   turnRight()
+   x=PiDrive()
+   x.forward()
+   x.backward()
+   x.turnLeft()
+   x.turnRight()
 
