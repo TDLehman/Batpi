9393381bbbe4616aed7b201fdfb28321032e3e00
Tylor Lehman <tylorlehman@yahoo.com>
2016-04-08 11:47:52 -0400

    add stepcounter to vlad

--- src/main/python/Vlad.py
+++ src/main/python/Vlad.py
@@ -14,11 +14,13 @@ def Vlad():
    
    envmap=sensor.sweep()
    front = len(envmap)/2
+   stepcount = 0 # How far vlad has travelled thus far.
    print "I'm going to try to go forward."
    while(envmap(front)>500 & envmap(front+1)>500 & envmap(front-1)>500):
       motor.forward()
       envmap=sensor.sweep()
+      stepcount=stepcount+1
    
    print "There's something in front of me. I'm done."
-
+   print "I performed "+stepcount+" actions!"
 Vlad()
