8d4edece31e656dde36e1dc904b37c5928f165b4
TylorLehman <tylorlehman@yahoo.com>
2016-04-08 21:45:59 +0000

    Extensive changes to Echolocate.py
    Error detection:
      retake samples if >5000
      retake samples if min = -1, this means echo didn't trigger
        -notified by loopbreaker being >1000
    Optimization:
      reworked distance equation to only take 1 cycle
      Removed sleep, instead wait for Echo pin to be low

--- src/main/python/Echolocate.py
+++ src/main/python/Echolocate.py
@@ -21,15 +21,14 @@ class Echolocate(object):
       y = self.takeSample(self.trig, self.echo)
       z = self.takeSample(self.trig, self.echo)
 
-      while max(x,y,z) > 2*min(x,y,z) or max(x,y,z)>10000:
-         print ("Outlier Detected. Retaking Samples")
+      while max(x,y,z) > 2*min(x,y,z) or max(x,y,z)>5000 or min(x,y,z)==-1:
+         #print ("Outlier Detected. Retaking Samples")
          x = self.takeSample(self.trig, self.echo)
          y = self.takeSample(self.trig, self.echo)
          z = self.takeSample(self.trig, self.echo)
 
       avDistance = (x+y+z)/float(3)
       return avDistance
-
 
    def takeSample(self, trig, echo):
       # Uncomment for error checking
@@ -38,16 +37,22 @@ class Echolocate(object):
       GPIO.setup(trig, GPIO.OUT) # set GPIO ports as either output or input
       GPIO.setup(echo, GPIO.IN)
       GPIO.output(trig,  False) # set trigger pin to low,
-      time.sleep(.1) # wait for it to settle
+      #time.sleep(.1) # wait for it to settle
       # Instead, just wait for echo pin to be low.
       while GPIO.input(echo) == 1:
          print "waiting for this thing to cool off"
       GPIO.output(trig, True)
       time.sleep(0.00001) # HC - SR04 documentation, set trig high 10uS.
       GPIO.output(trig, False)
+      loopbreaker=0
       start=time.time() # added this to prevent crash from undefined start
       while GPIO.input(echo) == 0: # trigger sets echo pin to high
-        start = time.time() # Keeps getting stuck here! 
+        start = time.time() # Keeps getting stuck here!
+        loopbreaker=loopbreaker+1
+        if loopbreaker>1000:
+          #print "Echo didn't trigger. Returning"
+          return -1
+          
         #print time.time()-start
       end=start # prevent crash from undefined end hopefully!
       while GPIO.input(echo) == 1: # echo goes low when signal is returned
@@ -59,11 +64,12 @@ class Echolocate(object):
 
 if __name__ == "__main__":
    y = Echolocate()
+   print y.getSample()
    # Try 100 Times
-   x = 0
-   while x<100:
-     if y.getSample()>150:
-       print "Something went wrong at "+x
-     x=x+1
-   print "done. did something go wrong? ^ "+x  
+   #x = 0
+   #while x<100:
+     #if y.getSample()>150:
+       #print "Something went wrong at "+x
+     #x=x+1
+   #print "done. did something go wrong? ^ "+x  
    
