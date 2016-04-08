0e15fb881809b5a8d5b57e0584a59e91ab822b90
TylorLehman <tylorlehman@yahoo.com>
2016-04-08 21:41:58 +0000

    Modify Vlad AI
    Takes a sample, then walks forward til he thinks he is near it
    then prints the number of "steps" it took

--- src/main/python/Vlad.py
+++ src/main/python/Vlad.py
@@ -4,23 +4,27 @@
 
 
 # Initial AI for BatPi. Will try to go forward until there is something 
-# 500 units in front of it.
+# 50 units in front of it.
 
 from PiDrive import PiDrive
-
+from fullsweep import fullsweep
 def Vlad():
    motor = PiDrive()
    sensor= fullsweep()
    
    envmap=sensor.sweep()
    front = len(envmap)/2
+   
    stepcount = 0 # How far vlad has travelled thus far.
    print "I'm going to try to go forward."
-   while(envmap(front)>500 & envmap(front+1)>500 & envmap(front-1)>500):
+   print "This is what I see: "+str(envmap[front-1])+", "+str(envmap[front])+", "+str(envmap[front])
+   print "Here is everything I see:"+str(envmap)
+   while(envmap[front]>50 and envmap[front+1]>50 and envmap[front-1]>50):
       motor.forward()
-      envmap=sensor.sweep()
       stepcount=stepcount+1
+      envmap[front]=envmap[front]-50
    
    print "There's something in front of me. I'm done."
-   print "I performed "+stepcount+" actions!"
+   print "I performed "+str(stepcount)+" actions!"
+   
 Vlad()
