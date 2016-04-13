eb0af7d9d0d4e28d2f0605b4c0b835db0d0dfe2e
TylorLehman <tylorlehman@yahoo.com>
2016-04-13 22:08:51 +0000

    Update userai
    Fixreverse to whatever backing up is called in motor class

--- src/main/python/UserAI.py
+++ src/main/python/UserAI.py
@@ -6,31 +6,31 @@
 # Initial AI for BatPi. Will try to go forward until there is something 
 # 500 units in front of it.
 
-#from PiDrive import PiDrive
-
+from PiDrive import PiDrive
+from fullsweep import fullsweep
 def UserAI():
-   #motor = PiDrive()
-   #sensor= fullsweep()
-   #envmap=sensor.sweep()
-   #front = len(envmap)/2
+   motor = PiDrive()
+   sensor= fullsweep()
+   envmap= sensor.sweep()
+   front = len(envmap)/2
    stepcount = 0 # How far user has travelled thus far.
    command=raw_input("What should I do? (w,a,s,d,x,q)")
    while(command=="w" or command=="a" or command=="s" or command=="d" or command=="x"):
       if command=="w":
          print "w"
-         #motor.forward()
+         motor.forward()
       elif command=="a":
          print "a"
-         #motor.turnLeft()
+         motor.turnLeft()
       elif command=="s":
          print "s"
-         #motor.reverse()
+         motor.reverse()
       elif command=="d":
          print "d"
-         #motor.turnRight()
+         motor.turnRight()
       elif command=="x":
          print "x"
-         #envmap=sensor.sweep()
+         envmap=sensor.sweep()
       command=raw_input("What should I do? (w,a,s,d,x,q)")    
       stepcount=stepcount+1
 
