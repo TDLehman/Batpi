0fc2d24fb475aaa382434e136632ef4ffd40a6e2
Tylor Lehman <tylorlehman@yahoo.com>
2016-04-13 11:41:46 -0400

    added AI that performs user designated input

---
+++ src/main/python/UserAI.py
@@ -0,0 +1,33 @@
+# Tylor Lehman
+# BatPi
+# UserAI.py
+
+
+# Initial AI for BatPi. Will try to go forward until there is something 
+# 500 units in front of it.
+
+from PiDrive import PiDrive
+
+def UserAI():
+   motor = PiDrive()
+   sensor= fullsweep()
+   envmap=sensor.sweep()
+   front = len(envmap)/2
+   stepcount = 0 # How far user has travelled thus far.
+   command=input("What should I do? (w,a,s,d,x,q)")
+   while(command==("w" or "a" or "s" or "d" or "x")):
+      if command=="w":
+         motor.forward()
+      elif command=="a":
+         motor.turnLeft()
+      elif command=="s":
+         motor.reverse()
+      elif command=="d":
+         motor.turnRight()
+      elif command=="x":
+         envmap=sensor.sweep()         
+      stepcount=stepcount+1
+
+   print "I performed "+stepcount+" actions!"
+
+UserAI()
