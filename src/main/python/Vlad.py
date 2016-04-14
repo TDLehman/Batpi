8e7fe114a906170afa99e29441f726e7b5694c96
Tylor Lehman <tylorlehman@yahoo.com>
2016-04-14 17:03:45 -0400

    Reworked Vlad very much. probs broken

--- src/main/python/Vlad.py
+++ src/main/python/Vlad.py
@@ -11,21 +11,50 @@ from fullsweep import fullsweep
 def Vlad():
    motor = PiDrive()
    sensor= fullsweep()
-   
+   direction = 0  #keep track of direction faced.
    envmap=sensor.sweep()
    front = len(envmap)/2
-   
+   fdistance = 0 # How far will I go per forward called?
    stepcount = 0 # How far vlad has travelled thus far.
    print "I'm going to try to go forward."
    print "This is what I see: "+str(envmap[front-1])+", "+str(envmap[front])+", "+str(envmap[front+1])
-   print "Here is everything I see:"+str(envmap)
-   while(envmap[front]>20 and envmap[front+1]>20 and envmap[front-1]>20):
-      motor.forward(min(envmap[front],envmap[front+1],envmap[front-1])/10)
+   #print "Here is everything I see:"+str(envmap)
+   prevmap=envmap
+
+   # This block calibrates forward distance
+   if(envmap[front]>10 and envmap[front+1]>10 and envmap[front-1]>10):
+      print "Calibrating"
+      sample=sensor.checkFront()
+      motor.forward()
+      fdistance=sample-sensor.checkFront()
       envmap=sensor.sweep()
-      stepcount=stepcount+1
+
+
       
-   
-   print "There's something in front of me. I'm done."
+   while(stepcount<10):   
+      while(envmap[front]>20 and envmap[front+1]>20 and envmap[front-1]>20):
+         motor.forward()
+         motor.forward()
+         envmap=sensor.sweep()
+         stepcount=stepcount+1
+      if(envmap[front-3]>envmap[front+3] and envmap[0]>20):
+         motor.turnRight()
+         motor.turnRight()
+         motor.forward()
+         motor.forward()
+         motor.turnLeft()
+         motor.turnLeft()
+         stepcount=stepcount+1
+      elif(envmap[10]>20):
+         motor.turnLeft()
+         motor.turnLeft()
+         motor.forward()
+         motor.forward()
+         motor.turnRight()
+         motor.turnRight()
+         stepcount=stepcount+1
+   print "I think I'm pretty stuck. I'm done."
    print "I performed "+str(stepcount)+" actions!"
+
    
 Vlad()
