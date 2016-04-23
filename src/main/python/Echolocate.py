901cd7c70593fbf489cb19c708a61203255ac579
TylorLehman <tylorlehman@yahoo.com>
2016-04-23 14:55:56 +0000

    echolocate is functioning pretty well right now

--- src/main/python/Echolocate.py
+++ src/main/python/Echolocate.py
@@ -23,7 +23,7 @@ class Echolocate(object):
 
 
       # Check for outliers
-      while max(x,y,z) > 1.5*min(x,y,z) or min(x,y,z)==-1:
+      while max(x,y,z) > 1.25*min(x,y,z) or min(x,y,z)==-1:
          print ("Outlier Detected. Retaking Samples")
          x = self.takeSample(self.trig, self.echo)
          y = self.takeSample(self.trig, self.echo)
@@ -47,20 +47,21 @@ class Echolocate(object):
       time.sleep(0.00001) # HC - SR04 documentation, set trig high 10uS.
       GPIO.output(trig, False)
       loopbreaker=0
-      start=time.time() # added this to prevent crash from undefined start
+      #start=time.time() # added this to prevent crash from undefined start
       while GPIO.input(echo) == 0: # trigger sets echo pin to high
         start = time.time() # Keeps getting stuck here!
         loopbreaker=loopbreaker+1
-        if loopbreaker>1000:  # This would indicate that Echo didn't trigger
+        #if loopbreaker>1000:  # This would indicate that Echo didn't trigger
           #print "Echo didn't trigger. Returning"
-          return -1
+         # return -1
           
         #print time.time()-start
-      end=start # prevent crash from undefined end hopefully!
+      #end=start # prevent crash from undefined end hopefully!
       while GPIO.input(echo) == 1: # echo goes low when signal is returned
         end = time.time() # Python returns last time echo was high 
       duration = end - start # measure the time the pin stayed high
       distance = duration*17150 # Will return Distance in CM
+      #Not: 17150 because speed of sound/2
       # reset pins, return distance to nearest object
       return distance
 
